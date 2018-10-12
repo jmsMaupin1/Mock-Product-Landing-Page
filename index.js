146d6ad0f45eb8c5cf96083e71452ee6c0d23ddb
JmsMaupin1 <jmsMaupin1@yahoo.com>
2018-10-11 21:31:42 -0400

    Add button to test for viewport width

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-  $("button").click(()=>alert($(window).width()))
+  $("button").click(()=>{
+    $(".home-content > h1").text($(window).width())
+  })
 	// Smooth Scrolling - Courtesy of css-tricks
 	// Select all links with hashes
 $('a[href*="#"]')
